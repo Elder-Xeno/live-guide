34f8a99bae3019b81ff5e73c65629eafea98b0f0
Gabby <gabzo24@live.com.au>
2024-05-09 16:55:55 +1000

    rearrange form fields

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -5,9 +5,9 @@ export default class SignUpForm extends Component {
   state = {
     name: '',
     email: '',
+    location: '',
     password: '',
     confirm: '',
-    location: '',
     error: ''
   };
 
@@ -21,8 +21,8 @@ export default class SignUpForm extends Component {
   handleSubmit = async (evt) => {
     evt.preventDefault();
     try {
-      const {name, email, password, location} = this.state;
-      const formData = {name, email, password, location};
+      const {name, email, location, password} = this.state;
+      const formData = {name, email, location, password};
       // The promise returned by the signUp service
       // method will resolve to the user object included
       // in the payload of the JSON Web Token (JWT)
@@ -43,12 +43,12 @@ export default class SignUpForm extends Component {
             <input type="text" name="name" value={this.state.name} onChange={this.handleChange} required />
             <label>Email</label>
             <input type="email" name="email" value={this.state.email} onChange={this.handleChange} required />
+            <label>Location</label>
+            <input type="text" name="location" value={this.state.location} onChange={this.handleChange} required />
             <label>Password</label>
             <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required />
             <label>Confirm</label>
             <input type="password" name="confirm" value={this.state.confirm} onChange={this.handleChange} required />
-            <label>Location</label>
-            <input type="text" name="location" value={this.state.confirm} onChange={this.handleChange} required />
             <button type="submit" disabled={disable}>SIGN UP</button>
           </form>
         </div>
