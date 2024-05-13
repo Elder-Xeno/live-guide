115870aec075d3a2957046a59b9e0273409428e8
mathewhooler <149305887+mathewhooler@users.noreply.github.com>
2024-05-13 23:39:54 +1000

    EventPost.jsx edits

--- src/components/EventPost/EventPost.jsx
+++ src/components/EventPost/EventPost.jsx
@@ -3,19 +3,14 @@ import './EventPost.css';
 export default function EventPost({ eventPost }) {
     return (
         <div className="event-post">
-<<<<<<< HEAD
-            <h2>{eventPost.title}</h2>
-=======
-            <p>Posted by: {eventPost.user.name}</p>
-            <h3>{eventPost.title}</h3>
->>>>>>> 2459cfa9d124ea49ef222ddad322f72035380297
+            <><h2>{eventPost.title}</h2>
             <p>Description: {eventPost.description}</p>
             <p>Date: {eventPost.date}</p>
             <p>Venue: {eventPost.venue}</p>
             <p>Price: {eventPost.price}</p>
             <p>Supporting Acts: {eventPost.supportingActs.join(', ')}</p>
             <p>Spotify Link: <a href={eventPost.spotifyLink}>{eventPost.spotifyLink}</a></p>
-            <p>Ticket Link: <a href={eventPost.ticketLink}>{eventPost.ticketLink}</a></p>
+            <p>Ticket Link: <a href={eventPost.ticketLink}>{eventPost.ticketLink}</a></p></>
         </div>
     );
 }
